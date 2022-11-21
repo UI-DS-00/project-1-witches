eec4a2fce9a698373a63bd45514f32c59560d2db
FatemeDallal <108277235+FatemeDallal@users.noreply.github.com>
2022-11-21 14:23:52 +0330

    st

--- app/src/main/java/com/example/imdb/StartActivity2.kt
+++ app/src/main/java/com/example/imdb/StartActivity2.kt
@@ -1,51 +1,11 @@
 package com.example.imdb
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.example.imdb.databinding.ActivityStart2Binding
-import java.util.Timer
-import kotlin.concurrent.timerTask
 
 class StartActivity2 : AppCompatActivity() {
-
-    private lateinit var binding : ActivityStart2Binding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityStart2Binding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-
-        var textArray = arrayOf(
-            "Movies",
-            "TV Shows",
-            "Awards & Events",
-            "Celebs",
-            "Find all of them in IMDb!",
-            "Lets go!"
-        )
-
-        var i = 1;
-        for (i in 1..6){
-            helpTextGenerator((i * 1000).toLong(), textArray[i - 1])
-        }
-
-        // for start page
-        Timer().schedule(timerTask {
-         startActivity(Intent(this@StartActivity2, MainActivity::class.java))
-         finish()
-        }, 7000)
-    }
-
-    private fun helpTextGenerator(delayTime:Long, helpText:String)
-    {
-        Timer().schedule(timerTask {
-           runOnUiThread(timerTask {
-               binding.helpText.text = helpText
-           })
-        }, delayTime)
+        setContentView(R.layout.activity_start2)
     }
 }
